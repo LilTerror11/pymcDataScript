26bcbdd5d77a961edd8ecd5367638b68b888d6d6
SmoothTurtle872 <whatleyfamily1@gmail.com>
2024-10-02 14:31:52 +0930

    Updated parser

--- parser.py
+++ parser.py
@@ -1,7 +1,21 @@
+import re
+
+
+def getFuncParams(line: str) -> str:
+    return re.search(r"\(\"(.*)\"\)", line).group(1)
+
 def parseFile(path: str):
     with open(path) as f:
-        code = f.read()
+        code = f.read().split("\n")
+
+    lines = code
+    for index, line in enumerate(lines):
+        if line == "":
+            code.pop(index)
+
+    for line in code:
+        if re.search(r"print\(", line):
+            print(f"say {getFuncParams(line)}")
 
 
-
-parseFile("main.py")
+parseFile("test.pmds")
